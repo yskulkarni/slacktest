539a11e2467c5332c1c87327b1ccc9d62726e0cf
unknown <482443@LT036512.cts.com>
2016-05-17 14:32:59 +0530

    Kick started development with HelloWorld.

---
+++ src/in/gnosko/slacktest/HelloWorld.java
@@ -0,0 +1,14 @@
+package in.gnosko.slacktest;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: 482443
+ * Date: 5/17/16
+ * Time: 5:56 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorld {
+    public static void main() {
+        System.out.println("Hellow World!");
+    }
+}
